57a52aa74a13db8e39c7c992f26fa026d78eb4af
olya <olya@ivanovs.info>
2021-08-14 19:31:47 +1200

    Add methods for REST API.

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets, response, status
+from rest_framework.views import APIView
+from .models import Product, User
+from .serializers import ProductSerializer
 
-# Create your views here.
+import random
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products GET
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return response.Response(serializer.data)
+
+    def create(self, request):  # /api/products POST
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):  # /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return response.Response(serializer.data)
+
+    def update(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return response.Response({"id": user.id})
